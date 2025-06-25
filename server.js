748a9e37d2d06c0e53aa72b95c19a6266367cec6
inntenz <141833758+inntenz@users.noreply.github.com>
2025-06-25 13:06:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+const https = require("https");
+
+const url = "https://rendertest-oqbw.onrender.com";
+
+setInterval(() => {
+    https.get(url, (res) => {
+        console.log(`Pinged ${url}: Status ${res.statusCode}`);
+    }).on("error", (err) => {
+        console.error(`Error pinging ${url}:`, err.message);
+    });
+}, 10000); // Alle 10 Sekunden
+
+
 const express = require("express");
 const axios = require("axios"); // Zum Abrufen der API-Daten
 const app = express();
